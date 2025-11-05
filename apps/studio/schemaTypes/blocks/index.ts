8fb8b818ebbc7a563e0f67499b0daa51cd7906c9
SameerJS6 <singh6.sameer@gmail.com>
2025-11-05 22:19:30 +0530

    Add mainColumn and jambFooter to exports: include mainColumn in pageBuilderBlocks and jambFooter in singletons for enhanced layout and document structure.

--- apps/studio/schemaTypes/blocks/index.ts
+++ apps/studio/schemaTypes/blocks/index.ts
@@ -3,6 +3,7 @@ import { faqAccordion } from "./faq-accordion";
 import { featureCardsIcon } from "./feature-cards-icon";
 import { hero } from "./hero";
 import { imageLinkCards } from "./image-link-cards";
+import { mainColumn } from "./main-column";
 import { subscribeNewsletter } from "./subscribe-newsletter";
 
 export const pageBuilderBlocks = [
@@ -11,5 +12,6 @@ export const pageBuilderBlocks = [
   featureCardsIcon,
   faqAccordion,
   imageLinkCards,
+  mainColumn,
   subscribeNewsletter,
 ];
--- apps/studio/schemaTypes/documents/index.ts
+++ apps/studio/schemaTypes/documents/index.ts
@@ -4,11 +4,12 @@ import { blogIndex } from "./blog-index";
 import { faq } from "./faq";
 import { footer } from "./footer";
 import { homePage } from "./home-page";
+import { jambFooter } from "./jamb-footer";
 import { navbar } from "./navbar";
 import { page } from "./page";
 import { redirect } from "./redirect";
 import { settings } from "./settings";
 
-export const singletons = [homePage, blogIndex, settings, footer, navbar];
+export const singletons = [homePage, blogIndex, settings, footer, navbar, jambFooter];
 
 export const documents = [blog, page, faq, author, ...singletons, redirect];
